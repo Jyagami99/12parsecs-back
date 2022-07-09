f49f9ab8e6f186272b53ad2b8e5c00a105e77778
Joao Marcelo <jm.castro@usp.br>
2022-07-08 21:09:43 -0300

    chore: qa is done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRouter from "../routes/authRouter.js"
+import authRouter from "./routes/authRouter.js";
 
 async function main() {
   dotenv.config();
 
   const app = express();
 
-  app.use(cors);
-  app.use(json);
+  app.use(cors());
+  app.use(json());
 
   app.use(authRouter);
 
