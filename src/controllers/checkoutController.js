99111818aab6ed72826171fb72e4c32a323ad41d
Carla <carla.dayane.berredo@gmail.com>
2022-07-11 16:03:59 -0400

    feat: checkout

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,16 +1,28 @@
 import { db } from "../db/database.js";
 
 export async function getCheckout(request, response) {
+  const {user} = response.locals;
   try {
-    const checkout = await db.collection("checkout").find().toArray();
+    const checkout = await db.collection("checkout").find({userId: user._id}).toArray();
     response.send(checkout);
   } catch (error) {
     console.log("checkout error", error);
-    return res.sendStatus(500);
+    return response.sendStatus(500);
   }
 }
 
 export async function postCheckout(request, response) {
+  const {user} = response.locals;
+  const products = request.body;
+
+  try{
+    await db.collection('checkout').insertOne({...products, userId: user._id});
+    response.sendStatus(200);
+  }
+  catch(erro){
+      console.log('erro ao inserir um registro',erro);
+  }
+
   // try {
   //   const product = request.body;
   //   await db.collection("checkout").insertOne({
--- src/routes/checkoutRouter.js
+++ src/routes/checkoutRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getCheckout, postCheckout } from "../controllers/checkoutController.js";
 
 const checkoutRouter = Router();
-checkoutRouter.get("/checkout");
-checkoutRouter.post("/checkout");
+checkoutRouter.get("/checkout", getCheckout);
+checkoutRouter.post("/checkout", postCheckout);
 checkoutRouter.delete("/checkout")
 
 export default checkoutRouter;
